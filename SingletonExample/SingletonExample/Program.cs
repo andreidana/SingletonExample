a7fcaa506f7d68d4e8172f93a98342fb49808f27
Ardeyu <andrei.dana87@gmail.com>
2018-03-12 22:55:09 +0200

    Add singleton design implementation

---
+++ SingletonExample/SingletonExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singletonInstance = SingletonDesignPattern.GetInstance();
+
+            Console.WriteLine(singletonInstance.GetInstanciatedVariable());
+        }
+    }
+}
---
+++ SingletonExample/SingletonExample/SingletonDesignPattern.cs
@@ -0,0 +1,36 @@
+﻿namespace SingletonExample
+{
+    public class SingletonDesignPattern
+    {
+        private static volatile SingletonDesignPattern _singletonInstance;
+        private static readonly object LockObject = new object();
+        private readonly int _instantiatedVariable;
+
+        private SingletonDesignPattern()
+        {
+            _instantiatedVariable = 1;
+        }
+
+        public static SingletonDesignPattern GetInstance()
+        {
+            //check for null instance to avoid always creating the lock if we already have an instance
+            if (_singletonInstance == null)
+            {
+                lock (LockObject)
+                {
+                    if (_singletonInstance == null)
+                    {
+                        _singletonInstance = new SingletonDesignPattern();
+                    }
+                }
+            }
+
+            return _singletonInstance;
+        }
+
+        public int GetInstanciatedVariable()
+        {
+            return _instantiatedVariable;
+        }
+    }
+}
